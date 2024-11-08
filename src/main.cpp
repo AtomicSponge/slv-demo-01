ded3b95e1f8ef58221ca8a425f2caad40223a64d
Matthew Evans <mevans+git@wtfsystems.net>
2024-11-08 13:45:15 -0500

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -670,32 +670,6 @@ int main(int argc, char **argv) {
   wte::engine::add_scene<title_scene>();
   wte::engine::add_scene<game_scene>();
 
-  wte::engine::new_game = [](){
-    //  Spawn starting entities
-    wte::mgr::spawner::spawn("starfield", {});
-    wte::mgr::spawner::spawn("score_overlay", {});
-    wte::mgr::spawner::spawn("player_info_overlay", {});
-    wte::mgr::spawner::spawn("game_over_overlay", {});
-    wte::mgr::spawner::spawn("player", {});
-    wte::mgr::spawner::spawn("main_cannon", {});
-    wte::mgr::spawner::spawn("shield", {});
-
-    //  Reset score.
-    wte::mgr::variables::set("score", (int64_t)0);
-
-    //  Set number of lives.
-    if (wte::mgr::variables::get<int64_t>("max_lives") > 5 || wte::mgr::variables::get<int64_t>("max_lives") < 3)
-      wte::mgr::variables::set("max_lives", (int64_t)3);
-    wte::mgr::variables::set("lives", wte::mgr::variables::get<int64_t>("max_lives"));
-
-    wte::mgr::audio::music::a::play(wte::mgr::assets::get<ALLEGRO_AUDIO_STREAM>("music"));
-  };
-
-  wte::engine::end_game = [](){
-    if (wte::mgr::variables::get<int64_t>("score") > wte::mgr::variables::get<int64_t>("hiscore"))
-      wte::mgr::variables::set("hiscore", wte::mgr::variables::get<int64_t>("score"));
-  };
-
   wte::engine::on_engine_pause = [](){
     wte::mgr::audio::music::a::pause();
     wte::mgr::audio::ambiance::pause();
