cffea7260982b386b242473025eb65b7ad3b761a
Matthew Evans <mevans+git@wtfsystems.net>
2024-11-18 06:58:56 -0500

    sync

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -35,18 +35,20 @@ emcmake cmake .. \
 cmake --build . -t slvdemo
 
 #  Copy assets
+ASSETS_DIR=assets
+BUILD_DIR=build_emscripten
 cd ..
-cp -r assets/asteroid.png build_emscripten
-cp -r assets/background_control.sdf build_emscripten
-cp -r assets/cannon.png build_emscripten
-cp -r assets/explosion.wav build_emscripten
-cp -r assets/game_over.png build_emscripten
-cp -r assets/game.sdf build_emscripten
-cp -r assets/icon.bmp build_emscripten
-cp -r assets/laser.wav build_emscripten
-cp -r assets/music.ogg build_emscripten
-cp -r assets/shield.png build_emscripten
-cp -r assets/shield.wav build_emscripten
-cp -r assets/ship.png build_emscripten
-cp -r assets/spawn_asteroids.sdf build_emscripten
-cp -r assets/title.bmp build_emscripten
+cp -r ${ASSETS_DIR}/asteroid.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/background_control.sdf ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/cannon.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/explosion.wav ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/game_over.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/game.sdf ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/icon.bmp ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/laser.wav ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/music.ogg ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/shield.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/shield.wav ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/ship.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/spawn_asteroids.sdf ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/title.bmp ${BUILD_DIR}
