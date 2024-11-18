7581191c0a45f65df5ea9bad0890d790c908b87d
Matthew Evans <mevans+git@wtfsystems.net>
2024-11-18 07:22:21 -0500

    sync

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -3,6 +3,25 @@
 set -e
 
 mkdir -p build_emscripten
+
+#  Copy assets
+ASSETS_DIR=assets
+BUILD_DIR=build_emscripten
+cp -r ${ASSETS_DIR}/asteroid.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/background_control.sdf ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/cannon.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/explosion.wav ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/game_over.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/game.sdf ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/icon.bmp ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/laser.wav ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/music.ogg ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/shield.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/shield.wav ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/ship.png ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/spawn_asteroids.sdf ${BUILD_DIR}
+cp -r ${ASSETS_DIR}/title.bmp ${BUILD_DIR}
+
 cd build_emscripten
 
 USE_FLAGS=(
@@ -33,22 +52,3 @@ emcmake cmake .. \
   -D CMAKE_EXECUTABLE_SUFFIX_CXX=".html"
 
 cmake --build . -t slvdemo
-
-#  Copy assets
-ASSETS_DIR=assets
-BUILD_DIR=build_emscripten
-cd ..
-cp -r ${ASSETS_DIR}/asteroid.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/background_control.sdf ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/cannon.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/explosion.wav ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/game_over.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/game.sdf ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/icon.bmp ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/laser.wav ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/music.ogg ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/shield.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/shield.wav ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/ship.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/spawn_asteroids.sdf ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/title.bmp ${BUILD_DIR}
