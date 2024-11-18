5cc4e59b5a5c8dadd3bf7e5a9975e0c862d8fb99
Matthew Evans <mevans+git@wtfsystems.net>
2024-11-18 08:53:32 -0500

    sync

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -2,27 +2,29 @@
 
 set -e
 
-mkdir -p build_emscripten
-
-#  Copy assets
 ASSETS_DIR=assets
 BUILD_DIR=build_emscripten
-cp -r ${ASSETS_DIR}/asteroid.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/background_control.sdf ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/cannon.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/explosion.wav ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/game_over.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/game.sdf ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/icon.bmp ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/laser.wav ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/music.ogg ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/shield.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/shield.wav ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/ship.png ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/spawn_asteroids.sdf ${BUILD_DIR}
-cp -r ${ASSETS_DIR}/title.bmp ${BUILD_DIR}
 
-cd build_emscripten
+mkdir -p ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}/${ASSETS_DIR}
+
+#  Copy assets
+cp -r ${ASSETS_DIR}/asteroid.png ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/background_control.sdf ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/cannon.png ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/explosion.wav ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/game_over.png ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/game.sdf ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/icon.bmp ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/laser.wav ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/music.ogg ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/shield.png ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/shield.wav ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/ship.png ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/spawn_asteroids.sdf ${BUILD_DIR}/${ASSETS_DIR}
+cp -r ${ASSETS_DIR}/title.bmp ${BUILD_DIR}/${ASSETS_DIR}
+
+cd ${BUILD_DIR}
 
 USE_FLAGS=(
   -s USE_FREETYPE=1
