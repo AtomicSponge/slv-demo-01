62511d49fe8de230cb2204340e46b0ec2f434f95
Matthew Evans <mevans+git@wtfsystems.net>
2024-11-18 06:45:17 -0500

    sync

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -36,17 +36,17 @@ cmake --build . -t slvdemo
 
 #  Copy assets
 cd ..
-cp -r assets/asteroid.png build
-cp -r assets/background_control.sdf build
-cp -r assets/cannon.png build
-cp -r assets/explosion.wav build
-cp -r assets/game_over.png build
-cp -r assets/game.sdf build
-cp -r assets/icon.bmp build
-cp -r assets/laser.wav build
-cp -r assets/music.ogg build
-cp -r assets/shield.png build
-cp -r assets/shield.wav build
-cp -r assets/ship.png build
-cp -r assets/spawn_asteroids.sdf build
-cp -r assets/title.bmp build
+cp -r assets/asteroid.png build_emscripten
+cp -r assets/background_control.sdf build_emscripten
+cp -r assets/cannon.png build_emscripten
+cp -r assets/explosion.wav build_emscripten
+cp -r assets/game_over.png build_emscripten
+cp -r assets/game.sdf build_emscripten
+cp -r assets/icon.bmp build_emscripten
+cp -r assets/laser.wav build_emscripten
+cp -r assets/music.ogg build_emscripten
+cp -r assets/shield.png build_emscripten
+cp -r assets/shield.wav build_emscripten
+cp -r assets/ship.png build_emscripten
+cp -r assets/spawn_asteroids.sdf build_emscripten
+cp -r assets/title.bmp build_emscripten
