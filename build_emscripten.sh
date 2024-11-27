f728aed78a5857d948d230b368a6981e6ea621ee
Matthew Evans <mevans+git@wtfsystems.net>
2024-11-27 14:18:57 -0500

    sync

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -33,7 +33,6 @@ USE_FLAGS=(
   --use-port=libjpeg
   --use-port=sdl2
   --use-port=libpng
-  -s FULL_ES2=1
   -O3
 )
 
@@ -49,7 +48,7 @@ emcmake cmake .. \
   -D SDL2_INCLUDE_DIR="$SDL2_INCLUDE_DIR" \
   -D CMAKE_C_FLAGS="${USE_FLAGS[*]}" \
   -D CMAKE_CXX_FLAGS="${USE_FLAGS[*]}" \
-  -D CMAKE_EXE_LINKER_FLAGS="${USE_FLAGS[*]} --preload-file ${ASSETS_DIR}@" \
+  -D CMAKE_EXE_LINKER_FLAGS="${USE_FLAGS[*]} -s FULL_ES2=1 --preload-file ${ASSETS_DIR}@" \
   -D CMAKE_EXECUTABLE_SUFFIX_CXX=".html"
 
 cmake --build . -t slvdemo
